e81c6aec1c52b8aa13b57c642315cde7dc0d800e
gazinaft <gazinaft@gmail.com>
2020-10-25 15:31:46 +0200

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const openNav = () => {
+  document.getElementById("myNav").style.width = "100%";
+}
+  
+const closeNav = () => {
+  document.getElementById("myNav").style.width = "0%";
+}
